6cc5364309c816d102e8a398f8d6fec1f29b816d
eafit-201627501010 <30695124+eafit-201627501010@users.noreply.github.com>
2017-10-16 20:48:47 -0500

    Add files via upload

---
+++ laboratorios/lab01/codigo/Ejercicio2.py
@@ -0,0 +1,45 @@
+import DigraphAM
+
+   # @clase Ejercicio2
+    #Descripcion: implementacion de metodo para colorear grafo.
+    # @author David Sanchez Arboleda / Estefania Zapata
+  #  @version 1
+
+
+
+def bipartito(grafo, vertice):
+	color=[-1]*len(grafo)
+	color[vertice]=1
+
+	vertices=[]
+	vertices.append(vertice)
+
+	while vertices:
+		u=vertices.pop()
+		if grafo[u][u]==1:
+			return False
+
+		for v in range(len(grafo)):
+			if grafo[u][v] == 1 and color[v] == -1:
+				color[v]=1-color[u]
+				vertices.append(v)
+			elif grafo[u][v] == 1 and color[v] == color[u]:
+				return False
+	return True
+
+ c="y"
+   # Creacion del grafo con matrices
+      while c=="y" or c=="Y":
+	vertices=input("vertices: ")
+	arcos=input("arcos: ")
+	grafo2 = DigraphAM.digraphAM(int(vertices)-1)
+	for i in range (int(arcos)):
+		arco=input("arco: ")
+		grafo2.add_arc(int(arco[0]),int(arco[1]))
+		grafo2.add_arc(int(arco[1]),int(arco[0]))
+
+	if bipartito(grafo2.matriz,0):
+		print ("BICOLORABLE")
+	else:
+		print ("NOT BICOLORABLE")
+	c=input("continue? ")
