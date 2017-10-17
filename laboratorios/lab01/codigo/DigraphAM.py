9e983876da37559c84bea148d3d5a34ccba7f3cc
eafit-201627501010 <30695124+eafit-201627501010@users.noreply.github.com>
2017-10-16 20:03:01 -0500

    Add files via upload

---
+++ laboratorios/lab01/codigo/DigraphAM.py
@@ -0,0 +1,34 @@
+import numpy
+
+   # @clase digraphAl
+   # Descripcion: implementacion de grafo con matrices.
+   # @author David Sanchez Arboleda &  Estefania Zapata
+   # @version 1.0
+
+
+class digraphAM:
+
+    def __init__(self,size):
+        self.size = size
+        self.matriz = numpy.zeros(shape=(size+1,size+1))
+
+
+    def add_arc(self,source,destination,weight=1):
+        self.matriz[source,destination]=weight
+
+
+    def get_successors(self,vertex):
+    	#Crear una lista
+        successors=[]
+       
+        for i in range(self.size+1):
+        	
+            if self.matriz[vertex,i] != 0:
+            	#Agrego el vertice con el que se encontro el arco a la lista
+                successors.append (i)
+        #retorno la lista
+        return successors
+
+
+    def get_weight(self,source,destination):
+        return self.matriz[source][destination]
