612d6a792b1fff383dccdb7a5c7517312d11a27f
eafit-201627501010 <30695124+eafit-201627501010@users.noreply.github.com>
2017-10-19 16:28:50 -0500

    Update DigraphAL.py

--- laboratorios/lab03/codigo/DigraphAL.py
+++ laboratorios/lab03/codigo/DigraphAL.py
@@ -1,40 +1,40 @@
 "" "
     @class digraphAl
     Descripcion: implementacion de grafo con listas.
-    @author David Sanchez Arboleda - Estefania Zapata
+    @author David Sanchez Arboleda - Estefania Zapata - Jacobo Henao
     @version 1.2
 "" "
 
 
 clase  digraphAL :
 
-    def  __init__ ( auto , tamaño ):
-        auto . tamaño = tamaño
+    def  __init__ ( auto , tamaÃ±o ):
+        auto . tamaÃ±o = tamaÃ±o
         # Creo una lista
         self .lista = []
-        # Recorro desde la posición 0 hasta el tamaño asignado creando listas en cada posición
-        para i en el  rango (tamaño + 1 ): self .lista.append ([])
+        # Recorro desde la posiciÃ³n 0 hasta el tamaÃ±o asignado creando listas en cada posiciÃ³n
+        para i en el  rango (tamaÃ±o + 1 ): self .lista.append ([])
 
     def  add_arc ( self , source , destination , weight = 1 ):
-        # Añadimos una tupla en el vértice asignado que contiene el vértice adyacente con el peso de la esquina que por defecto es 1
+        # AÃ±adimos una tupla en el vÃ©rtice asignado que contiene el vÃ©rtice adyacente con el peso de la esquina que por defecto es 1
         self .lista [fuente] .append ((destino, peso))
 
  
-    def  get_successors ( self , vértice ):
+    def  get_successors ( self , vÃ©rtice ):
         # Creo una lista
         sucesores = []
-        # Recorro la lista de mi vértice especificado
-        para i en el  rango ( len ( self .lista [vértice])):
-            # Añado a mi lista creado todos los vértices adyacentes a mi vértice especificado
-            successors.append ( self .lista [vértice] [i] [ 0 ])
+        # Recorro la lista de mi vÃ©rtice especificado
+        para i en el  rango ( len ( self .lista [vÃ©rtice])):
+            # AÃ±ado a mi lista creado todos los vÃ©rtices adyacentes a mi vÃ©rtice especificado
+            successors.append ( self .lista [vÃ©rtice] [i] [ 0 ])
         # Retorno mi lista creado
         retorno sucesores
 
 
     def  get_weight ( self , source , destination ):
-        # Recorro la lista de mi vértice especificado
+        # Recorro la lista de mi vÃ©rtice especificado
         para i en el  rango ( len ( self .lista [fuente])):
-            # Si en la lista del vértice especificado se encuentra el vértice de destino retorno el peso del arco de los dos
+            # Si en la lista del vÃ©rtice especificado se encuentra el vÃ©rtice de destino retorno el peso del arco de los dos
             si  self .lista [fuente] [i] [ 0 ] == destino:
                 return  self .lista [fuente] [i] [ 1 ]
         # Retorno cero por defecto
