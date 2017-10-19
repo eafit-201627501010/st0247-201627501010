03c9c252d34dc7007ec5d0fb388b0b3f9352ffae
eafit-201627501010 <30695124+eafit-201627501010@users.noreply.github.com>
2017-10-19 17:10:37 -0500

    Add files via upload

---
+++ laboratorios/lab05/codigo/LongestCommonSubsequence.java
@@ -0,0 +1,24 @@
+public class LongestCommonSubsequence{
+
+
+public static int subsecuenciaComunMasLarga(String x, String y){
+	int m = x.length();
+	int n = y.length();
+	int[][] a = new int[m+1][n+1];
+ 
+	for(int i=0; i<=m; i++){
+		for(int j=0; j<=n; j++){
+			if(i==0 || j==0){
+				a[i][j]=0;
+			}else if(x.charAt(i-1)==y.charAt(j-1)){
+				a[i][j] = 1 + a[i-1][j-1];
+			}else{
+				a[i][j] = Math.max(a[i-1][j], a[i][j-1]);
+			}
+		}
+	}
+ 
+	return a[m][n];
+}
+
+}
